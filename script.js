38dab2472ee30e4c4258fcbd28957e11e1dd5393
Shami Sharma <shamisharma01@outlook.com>
2022-12-30 15:13:47 -0500

    Added generateTarger, compareGuesses and updateScore functions

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 9 + 1);
+}
+
+const compareGuesses = (humGuess, compGuess, target) => {
+    const humDiff = Math.abs(target - humGuess);
+    const compDiff = Math.abs(target - compGuess);
+
+   return humDiff <= compDiff
+}
+
+const updateScore = (winner) => {
+    switch (winner.toLowerCase()){
+        case 'human':
+            humanScore += 1;
+            break;
+        case 'computer':
+            computerScore += 1;
+            break;
+    }
+}
 
